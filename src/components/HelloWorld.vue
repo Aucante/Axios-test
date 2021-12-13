1b4acc48ee3077ab64af28cee449fd3bd9021ccc
Aucante <sebastien.aucante@gmail.com>
2021-12-14 00:08:48 +0100

    JSON Placeholder

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,41 @@
 <template>
   <div class="hello">
-    <p>ok</p>
+    <button v-on:click="createdPost()">MAJ</button>
+    <ul>
+      <li v-for="post in posts" :key="post">{{ post.title }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  data() {
+    return {
+      posts: ["post1", "post2", "post3"],
+    };
+  },
+  methods: {
+    updatePost() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/postsDD")
+        .then((response) => (this.posts = response.data))
+        .catch(
+          (error) => (this.posts = [{ title: "erreur chargeemnt" + error }])
+        );
+    },
+    createdPost() {
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", {
+          id: 101,
+          title: "foo",
+          body: "bar",
+          userId: 1,
+        })
+        .then((response) => console.log(response));
+    },
   },
 };
 </script>
